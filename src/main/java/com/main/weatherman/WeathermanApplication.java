5bc944bf2f02fee00296c103733811be8c98455a
Červinka <michal.cervinka@micronova.cz>
2023-05-22 23:52:05 +0200

    added initial spring template

---
+++ src/main/java/com/main/weatherman/WeathermanApplication.java
@@ -0,0 +1,13 @@
+package com.main.weatherman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathermanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathermanApplication.class, args);
+	}
+
+}
