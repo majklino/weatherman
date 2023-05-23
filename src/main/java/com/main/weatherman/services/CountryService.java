ba50725d4d5e5be1ec95914936617ac061786fbe
Červinka <michal.cervinka@micronova.cz>
2023-05-23 14:07:41 +0200

    added CountryService

---
+++ src/main/java/com/main/weatherman/services/CountryService.java
@@ -0,0 +1,28 @@
+package com.main.weatherman.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.main.weatherman.model.Country;
+import com.main.weatherman.repositories.CountryRepository;
+
+@Service
+public class CountryService {
+    private final CountryRepository repository;
+
+    @Autowired
+    public CountryService(CountryRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Country> getAllCountries(){
+        return repository.findAll();
+    }
+
+    public Country getCountryByName(String name){
+        return repository.findByName(name);
+    }
+}
+
