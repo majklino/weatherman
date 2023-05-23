6cd8cac6ae471ebe37a4396d51ab955a084695e1
Červinka <michal.cervinka@micronova.cz>
2023-05-23 14:08:19 +0200

    modified main

--- src/main/java/com/main/weatherman/WeathermanApplication.java
+++ src/main/java/com/main/weatherman/WeathermanApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeathermanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeathermanApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WeathermanApplication.class, args);
+    }
 }
