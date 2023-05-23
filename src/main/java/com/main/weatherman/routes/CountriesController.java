28353d8bf097f70f010571303157c3a9a9046cff
Červinka <michal.cervinka@micronova.cz>
2023-05-23 14:06:08 +0200

    added CountriesController

---
+++ src/main/java/com/main/weatherman/routes/CountriesController.java
@@ -0,0 +1,54 @@
+package com.main.weatherman.routes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.main.weatherman.model.Country;
+import com.main.weatherman.services.CountryService;
+
+@RestController
+@RequestMapping("/api/countries")
+public class CountriesController {
+    private final CountryService service;
+
+    @Autowired
+    public CountriesController(CountryService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Country> getAllCountries(){
+        return service.getAllCountries();
+    }
+
+    @GetMapping("/{name}")
+    public Country getCountryById(@PathVariable String name){
+        return service.getCountryByName(name);
+    }
+}
+
+// @RestController
+// @RequestMapping("/employees")
+// public class EmployeeController {
+//     private final EmployeeService employeeService;
+
+//     @Autowired
+//     public EmployeeController(EmployeeService employeeService) {
+//         this.employeeService = employeeService;
+//     }
+
+//     @GetMapping
+//     public List<Employee> getAllEmployees() {
+//         return employeeService.getAllEmployees();
+//     }
+
+//     @GetMapping("/{id}")
+//     public Employee getEmployeeById(@PathVariable int id) {
+//         return employeeService.getEmployeeById(id);
+//     }
+// }
